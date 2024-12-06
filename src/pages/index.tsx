7d7e6c235ee9ec925ad4f13c3bfef066ca96c53a
fscatv2016 <131167169+fscatv2016@users.noreply.github.com>
2024-12-06 16:52:59 +0800

    删除layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Layout from "src/layout/Layout";
 
 export const HomePage = () => {
   React.useEffect(() => {
     window.location.href = "/editor";
   }, []);
-  return <Layout></Layout>;
+  return null;
 };
 
 export default HomePage;
