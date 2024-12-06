e6eda856ef35016102ec4a57343e6f44587d2671
fscatv2016 <131167169+fscatv2016@users.noreply.github.com>
2024-12-06 16:42:51 +0800

    1、修改程序直接跳转至editor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,11 @@
 import React from "react";
-import type { InferGetStaticPropsType, GetStaticProps } from "next";
-import { NextSeo } from "next-seo";
-import { SEO } from "src/constants/seo";
-import { FAQ } from "src/containers/Landing/FAQ";
-import { Features } from "src/containers/Landing/Features";
-import { HeroPreview } from "src/containers/Landing/HeroPreview";
-import { HeroSection } from "src/containers/Landing/HeroSection";
-import { LovedBy } from "src/containers/Landing/LovedBy";
-import { Section1 } from "src/containers/Landing/Section1";
-import { SeePremium } from "src/containers/Landing/SeePremium";
 import Layout from "src/layout/Layout";
 
-export const HomePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return (
-    <Layout>
-      <NextSeo {...SEO} canonical="https://jsoncrack.com" />
-      <HeroSection />
-      <HeroPreview />
-      <Section1 />
-      <Features />
-      <FAQ />
-      <LovedBy stars={props.stars} />
-      <SeePremium />
-    </Layout>
-  );
+export const HomePage = () => {
+  React.useEffect(() => {
+    window.location.href = "/editor";
+  }, []);
+  return <Layout></Layout>;
 };
 
 export default HomePage;
-
-export const getStaticProps = (async () => {
-  try {
-    const res = await fetch("https://api.github.com/repos/AykutSarac/jsoncrack.com");
-    const data = await res.json();
-
-    return {
-      props: {
-        stars: data?.stargazers_count || 0,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        stars: 0,
-      },
-    };
-  }
-}) satisfies GetStaticProps<{ stars: number }>;
